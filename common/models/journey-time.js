72f195e614962bb349cd3112912fd82aa4ad2492
Guilherme Ribeiro de Melo Yabu <guilhermeyabu@inatel.br>
2018-10-29 16:33:26 -0300

    (feat) new endpoint to register point

--- common/models/journey-time.js
+++ common/models/journey-time.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Journeytime) {
+	
+	Journeytime.createPoint(data, cb) {
+		data.point = new Date()
+		Journeytime.create(data, function(errorCreate, instanceCreate) {
+			if (errorCreate) {
+				cb(errorCreate)
+			} else {
+				cb(null, "Ponto registrado com sucesso.")
+			}
+		}
+	}
+	
+	Journeytime.remoteMethod(
+    'createPoint',
+    {
+      http: { path: '/createPoint', verb: 'post' },
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+      returns: { arg: 'body', type: 'object', root: true },
+      description: "Registra um novo ponto."
+    }
+  );
 
 };
